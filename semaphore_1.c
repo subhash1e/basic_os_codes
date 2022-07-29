3665f46e3726f65a381e30b1345de1f519ebd503
Subhash Kumar <85139394+subhash1e@users.noreply.github.com>
2022-07-30 02:00:32 +0530

    Update semaphore_1.c

--- semaphore_1.c
+++ semaphore_1.c
@@ -1,1 +1,20 @@
 //first learn pthreads
+#include<pthread.h>
+#include<stdlio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<semaphore.h>
+
+#define TH_NUM 4
+
+sem_t semaphore;
+
+void* routine(void* args){
+//complete it to learn semaphores
+}
+
+int main(){
+
+  return 0;
+}
